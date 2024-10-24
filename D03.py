ce81659cdf5c869b300b4cb4756d5b460e540e20
izzie <izzienjeri12@gmail.com>
2024-10-24 03:59:55 +0300

    D04

---
+++ D03.py
@@ -0,0 +1,5 @@
+numbers = [12, 70, 9, 56, 24]
+
+largest_number = max(numbers)
+
+print(f"The largest number in the list is: {largest_number}")
---
+++ D04.py
@@ -0,0 +1,9 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+reversed = reverse_string("python")
+print(reversed)
+
+
+
+
