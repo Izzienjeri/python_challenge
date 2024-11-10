2804c2c76cb7775cee0eb1dd628cb4bc22892e37
izzie <izzienjeri12@gmail.com>
2024-11-10 21:45:58 +0300

    done

---
+++ D09.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry", "date", "blueberry"]
+
+# print(fruits[0])  
+# print(fruits[2])  
+
+# print(fruits[-1])  
+# print(fruits[-2])  
+
+# print(fruits[1:4])  # Output: ['banana', 'cherry', 'date']
+
+fruits.reverse()
+print(fruits)  # Reversed list
